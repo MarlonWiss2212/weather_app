b83c59e220562067f13fd112c4c369e3401d4fca
MarlonWiss2212 <marlon.wisskirchen@outlook.de>
2024-02-15 20:20:05 +0100

    add widget sunrise sunset

--- lib/features/weather/presentation/widgets/weather_sunrise_sunset.dart
+++ lib/features/weather/presentation/widgets/weather_sunrise_sunset.dart
@@ -1,1 +1,10 @@
-//TODO: implement component
+import 'package:flutter/material.dart';
+
+class WeatherSunriseSunset extends StatelessWidget {
+  const WeatherSunriseSunset({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
