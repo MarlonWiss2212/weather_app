f95c4cc5e701c513612038eb7ce0040c5c28e855
MarlonWiss2212 <marlon.wisskirchen@outlook.de>
2024-03-09 17:52:29 +0100

    add missing icons

--- lib/core/util/icon_utils.dart
+++ lib/core/util/icon_utils.dart
@@ -12,16 +12,18 @@ class IconUtils {
         "02d" => CupertinoIcons.cloud_sun_fill,
         "02n" => CupertinoIcons.cloud_moon_fill,
         "03d" || "04d" || "03n" || "04n" => CupertinoIcons.cloud_fill,
-        "09d" => snow
+        "09d" || "09n" => snow
             ? CupertinoIcons.cloud_snow_fill
             : CupertinoIcons.cloud_rain_fill,
-        "10d" => snow
+        "10d" || "10n" => snow
             //TODO: Cloud_sun_snow_fill in future
             ? CupertinoIcons.cloud_snow_fill
-            : CupertinoIcons.cloud_sun_rain_fill,
-        "11d" => CupertinoIcons.cloud_bolt_fill,
-        "13d" => CupertinoIcons.cloud_snow_fill,
-        "30d" => CupertinoIcons.cloud_fog_fill,
+            : iconCode == "10d"
+                ? CupertinoIcons.cloud_sun_rain_fill
+                : CupertinoIcons.cloud_moon_rain_fill,
+        "11d" || "11n" => CupertinoIcons.cloud_bolt_fill,
+        "13d" || "13n" => CupertinoIcons.cloud_snow_fill,
+        "50d" || "50n" => CupertinoIcons.cloud_fog_fill,
         _ => Icons.abc,
       };
 }
